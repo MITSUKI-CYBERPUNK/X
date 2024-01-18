f546732a4778531df0583ef2275fdb320b7f9531
MITSUKI-CYBERPUNK <52666044+MITSUKI-CYBERPUNK@users.noreply.github.com>
2024-01-18 12:06:26 +0800

    Delete code/240117 学习代码/list_comprehension.py

--- code/240117 学习代码/list_comprehension.py
+++
@@ -1,8 +0,0 @@
-# 生成1到10的平方列表
-squares = [x**2 for x in range(1, 11)]
-
-# 过滤偶数，生成仅包含奇数的列表
-odd_numbers = [x for x in range(1, 11) if x % 2 != 0]
-
-# 使用条件表达式生成包含奇偶性判断的列表
-evenodd_label = ["Even" if x % 2 == 0 else "Odd" for x in range(1, 11)]
