d4283892c11d10da58fb1380af0247dfa584c713
MITSUKI-CYBERPUNK <52666044+MITSUKI-CYBERPUNK@users.noreply.github.com>
2024-01-18 12:06:46 +0800

    Delete code/240117 学习代码/musicians.py

--- code/240117 学习代码/musicians.py
+++
@@ -1,7 +0,0 @@
-musicians = ['me', 'yoshiki', 'hyde']
-for musician in musicians:
-    if musician == 'me':
-        print(musician.upper())
-    else:
-        print(musician.title())
-# 同样地，不要漏掉冒号和缩进
